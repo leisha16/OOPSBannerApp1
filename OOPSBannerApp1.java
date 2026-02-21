1685b984ae73f687e3d775db80e6a714f557b8af
Leisha <leisha16saini@gmail.com>
2026-02-21 15:50:11 +0530

    Initial commit

---
+++ OOPSBannerApp1.java
@@ -0,0 +1,33 @@
+/**
+ 
+ * @author Leisha
+ * @version 2
+ */
+
+public class OOPSBannerApp1{
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(" *****    *****    *****    ***** ");
+
+        // Line 2
+        System.out.println("*     *  *     *  *     *  *      ");
+
+        // Line 3
+        System.out.println("*     *  *     *  *     *  *      ");
+
+        // Line 4
+        System.out.println("*     *  *     *  *****    ***** ");
+
+        // Line 5
+        System.out.println("*     *  *     *  *             *");
+
+        // Line 6
+        System.out.println("*     *  *     *  *             *");
+
+        // Line 7
+        System.out.println(" *****    *****   *        ***** ");
+
+    }
+}
